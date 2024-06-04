7c69d50ea0cb45408bc347da48b72964e192f0da
Mafupa <ryankarera@gmail.com>
2024-06-05 00:53:37 +0200

    0x16

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """number of subscribers"""
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {"User-Agent": "-"}
+    headers = {"User-Agent": "freline"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
