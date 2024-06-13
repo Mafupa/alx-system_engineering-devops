96b5ebeef5b8e6aa197bbac16de4a17227bfa33a
Mafupa <ryankarera@gmail.com>
2024-06-14 00:59:38 +0200

    0x1B Task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,12 @@
+# Increase request limit
+
+exec{ 'increase-limit':
+	command => 'sed -i "s/15/4096" /etc/default/nginx',
+	path => '/usr/local/bin/:/bin/'
+}
+
+# Restart
+exec{ 'restart':
+	command => 'nginx restart',
+	path => '/etc/init.d'
+}
